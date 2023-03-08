e74f9fe9a90db6cabccc19dcc871ea83a6021acd
shubham shirke <shirkeshubham84@gmail.com>
2023-03-08 23:08:59 +0530

    Added:Program to sort the array in ascending order

---
+++ src/AscendingArray.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class AscendingArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[5];
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Enter element for array: ");
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.print("The sorted array is: ");
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
