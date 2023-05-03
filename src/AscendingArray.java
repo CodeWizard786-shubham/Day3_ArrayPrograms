d6e7ba2a7f069e21aeb1d451d39efed619c40a9f
shubham shirke <shirkeshubham84@gmail.com>
2023-05-03 20:04:17 +0530

    Array programs

--- src/AscendingArray.java
+++ src/AscendingArray.java
@@ -4,13 +4,14 @@ public class AscendingArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int arr[] = new int[5];
+        int count=arr.length;
         for (int i = 0; i < 5; i++) {
             System.out.println("Enter element for array: ");
             arr[i] = sc.nextInt();
         }
         Arrays.sort(arr);
         System.out.print("The sorted array is: ");
-        for(int i = 0; i < arr.length; i++) {
+        for(int i = 0; i < count; i++) {
             System.out.print(arr[i] + " ");
         }
 
